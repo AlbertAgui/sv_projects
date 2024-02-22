b22306212c1289f8a564a7b6422426ca9de83e4d
aaguiler <albert.aguilera@bsc.es>
2024-02-22 20:31:32 +0100

    start script

---
+++ script/check_updated_tests/bash/get_updated_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export BASE_PATH=$(pwd)
+source get_updated_tests_var.sh
+
+#clone spike
+if [ ! -d "$REPO_ISS_FOLDER_PATH" ]; then
+    git clone $REPO_ISS_SSH
+    cd $REPO_ISS_FOLDER_PATH
+    git checkout $REPO_ISS_BRANCH
+    cd -
+fi
+
+#get files
+updated_tests=$(git diff --name-only HEAD HEAD~1)
+
+#run tests
+for test in updated_tests; do
+    echo $TEST_FOLDER_PATH
+    echo $test
+done
+
+#erase temp files
+#rm -rf $REPO_ISS_BRANCH
---
+++ script/check_updated_tests/bash/get_updated_tests_var.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TEST_FOLDER_PATH=$BASE_PATH/tests/
+
+REPO_ISS_SSH=git@gitlab-internal.bsc.es:eprocessor/eprocessor-rtl/spike.git
+REPO_ISS_BRANCH=feature/virtual_tests
+REPO_ISS_FOLDER_PATH=$BASE_PATH/spike/
