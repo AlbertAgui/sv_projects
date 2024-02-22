26b25a29d6da96545eb0b898fa72d8d20eb95577
aaguiler <albert.aguilera@bsc.es>
2024-02-22 20:46:14 +0100

    new test feature commit

--- script/check_updated_tests/bash/get_updated_tests.sh
+++ script/check_updated_tests/bash/get_updated_tests.sh
@@ -3,15 +3,15 @@ export BASE_PATH=$(pwd)
 source get_updated_tests_var.sh
 
 #clone spike
-if [ ! -d "$REPO_ISS_FOLDER_PATH" ]; then
-    git clone $REPO_ISS_SSH
-    cd $REPO_ISS_FOLDER_PATH
-    git checkout $REPO_ISS_BRANCH
-    cd -
-fi
+#if [ ! -d "$REPO_ISS_FOLDER_PATH" ]; then
+#    git clone $REPO_ISS_SSH
+#    cd $REPO_ISS_FOLDER_PATH
+#    git checkout $REPO_ISS_BRANCH
+#    cd -
+#fi
 
 #get files
-updated_tests=$(git diff --name-only HEAD HEAD~1)
+updated_tests=$(git diff --name-only HEAD~1 HEAD)
 
 #run tests
 for test in updated_tests; do
